cbecc79605778f9d8f0b593969dcc30f58328f96
Vladislav Chernyatevich <cvv.vlad@yandex.ru>
2022-07-10 13:45:22 +0300

    все задания

--- lesson-4/task_4.py
+++ lesson-4/task_4.py
@@ -4,3 +4,28 @@
 # Для выполнения задания обязательно используйте генератор.
 # Пример исходного списка: [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11].
 # Результат: [23, 1, 3, 10, 4, 11]
+import random
+
+
+# генератор уникальных записей
+def distinct_gen(input_list):
+    used_items = set()
+    for el in input_list:
+        if el not in used_items:
+            used_items.add(el)
+            yield el
+
+
+# генератор случайных чисел, сделал для эксперимента над большим объемом данных в разном порядке
+def random_range_gen(limit):
+    uniq_items = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+    for i in range(limit):
+        yield random.choice(uniq_items)
+
+
+# input_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]*10000
+input_list = [el for el in random_range_gen(100000)]
+result_list = [el for el in distinct_gen(input_list)]
+
+print(input_list)
+print(result_list)
--- lesson-4/task_6.py
+++ lesson-4/task_6.py
@@ -7,3 +7,19 @@
 #     Предусмотрите условие его завершения. #### Например, в первом задании выводим целые числа, начиная с 3.
 #     При достижении числа 10 — завершаем цикл. Вторым пунктом необходимо предусмотреть условие,
 #     при котором повторение элементов списка прекратится.
+
+from itertools import count, cycle
+
+# count
+count_iter = count(3)
+count_iter_list = []
+while len(count_iter_list) < 10:
+    count_iter_list.append(next(count_iter))
+print(count_iter_list)
+
+# cycle
+cycle_iter = cycle(count_iter_list)
+cycle_iter_list = []
+while len(cycle_iter_list) < 40:
+    cycle_iter_list.append(next(cycle_iter))
+print(cycle_iter_list)
